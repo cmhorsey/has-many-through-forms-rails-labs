45905781c799d2cc6cc7a51d1bf04e1cef12726c
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-15 14:14:56 -0400

    Create categories show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
-
   def show
     @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.find_or_create_by
   end
 end
